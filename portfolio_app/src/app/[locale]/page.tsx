3f80860556fcc8b149ed6cbc17527c661453d2df
firas <firas.moukhallalaty@gmail.com>
2025-10-13 01:11:20 +0200

    progress

--- portfolio_app/src/app/[locale]/page.tsx
+++ portfolio_app/src/app/[locale]/page.tsx
@@ -5,12 +5,32 @@ export default async function Home() {
 	return (
 		<>
 			<div className="homeContent">
-				<div className='page_firstPage'>
+				<div className='page page_firstPage'>
 					<div className="imgContainer">
-						<img src={homePageData.homePage_FirstPageSrc}/>
+						<img src={homePageData.firstPageSrc} />
 					</div>
-					<div className=''>
+					<div className='messageOverlayContainer'>
+						{
+							homePageData.firstPageWordsImgs.map((item, index) => {
+								return (
+									<div key={index} className={"imgContainer " + item.classname}>
+										<img src={item.src} />
+									</div>
+								)
+							})
+						}
+					</div>
+				</div>
+				<div className='page page_secondPage'>
+					<div className='flex--row'>
+						<div className='homePage_aboutMe'>
+							<div className='card'>
 
+							</div>
+						</div>
+						<div className='imgContainer portraitImg'>
+							<img src={homePageData.secondPage.imgSrc} />
+						</div>
 					</div>
 				</div>
 			</div>
